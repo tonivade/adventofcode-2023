1695df88b9f2bf79ca9208181938d582d3c7ad4e
Antonio Muñoz <antonio.munoz@clarity.ai>
2023-12-01 12:13:50 +0100

    initial commit

---
+++ src/main/scala/Day1.scala
@@ -0,0 +1,13 @@
+package day1
+
+import scala.io.Source
+
+object Day1:
+  def part1(input: String): Int = ???
+  def part2(input: String): Int = ???
+
+@main def main: Unit =
+  val input = Source.fromFile("input/day1.txt").getLines().mkString("\n")
+  println(Day1.part1(input))
+  println(Day1.part2(input))
+
