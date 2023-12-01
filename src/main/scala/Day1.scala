124369daf68137a3d5af94feebd473a17465a46c
Antonio Muñoz <antonio.munoz@clarity.ai>
2023-12-01 12:52:08 +0100

    day 1 part 1

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -3,11 +3,17 @@ package day1
 import scala.io.Source
 
 object Day1:
-  def part1(input: String): Int = ???
+  def part1(input: String): Int =
+    input.split("\n")
+      .map(line => line.filter(_.isDigit))
+      .map(line => s"${line.head}${line.last}")
+      .map(_.toInt)
+      .sum
+
   def part2(input: String): Int = ???
 
 @main def main: Unit =
   val input = Source.fromFile("input/day1.txt").getLines().mkString("\n")
   println(Day1.part1(input))
-  println(Day1.part2(input))
+//  println(Day1.part2(input))
 
--- src/test/scala/Day1Suite.scala
+++ src/test/scala/Day1Suite.scala
@@ -4,13 +4,24 @@ import Day1._
 
 class Day1Suite extends munit.FunSuite:
 
-  val input = """""".stripMargin
+  val input1 = """1abc2
+                 |pqr3stu8vwx
+                 |a1b2c3d4e5f
+                 |treb7uchet""".stripMargin
+
+  val input2 = """two1nine
+                 |eightwothree
+                 |abcone2threexyz
+                 |xtwone3four
+                 |4nineeightseven2
+                 |zoneight234
+                 |7pqrstsixteen"""
 
   test("Day1 part1") {
-    assertEquals(part1(input), 1)
+    assertEquals(part1(input1), 142)
   }
 
   test("Day1 part2") {
-    assertEquals(part2(input), 1)
+    assertEquals(part2(input2), 1)
   }
 
