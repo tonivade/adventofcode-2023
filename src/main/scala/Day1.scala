fdb04a2ec0c00cec9f1000b3240a8b68f93a88ae
Antonio Muñoz <antonio.munoz@clarity.ai>
2023-12-01 16:33:33 +0100

    day1 part 2 alternative solution

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -3,6 +3,9 @@ package day1
 import scala.io.Source
 
 object Day1:
+
+  val regex = "\\d|one|two|three|four|five|six|seven|eight|nine".r
+
   def part1(input: String): Int =
     input.split("\n")
       .map(line => line.filter(_.isDigit))
@@ -32,8 +35,23 @@ object Day1:
       case ((current, state), next) => (current, state + next)
     }._1
 
+  def parse2(line: String): String =
+    regex.findAllIn(line).map {
+      case "one" => "1"
+      case "two" => "2"
+      case "three" => "3"
+      case "four" => "4"
+      case "five" => "5"
+      case "six" => "6"
+      case "seven" => "7"
+      case "eight" => "8"
+      case "nine" => "9"
+      case digit => digit
+    }.mkString
+
 @main def main: Unit =
   val input = Source.fromFile("input/day1.txt").getLines().mkString("\n")
   println(Day1.part1(input))
   println(Day1.part2(input))
+  println(Day1.part1(input) + Day1.part2(input))
 
