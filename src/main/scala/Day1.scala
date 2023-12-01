053d670f064a076e8b5296047ae24a7b30174039
Antonio Muñoz <antonio.munoz@clarity.ai>
2023-12-01 14:08:00 +0100

    day 1 part 2 cleanup, it works with the example, but doesn't work with the input :facepalm:

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -12,31 +12,26 @@ object Day1:
 
   def part2(input: String): Int =
     input.split("\n")
-      .map(line => replaceAll(line))
-      .map(line => {
-        println(line)
-        line
-      })
-      .map(line => line.filter(_.isDigit))
+      .map(replaceAll)
       .map(line => s"${line.head}${line.last}")
       .map(_.toInt)
       .sum
 
   def replaceAll(line: String): String =
-    val result = line.foldLeft(("", "")) {
-      case ((current, state), next) if ((state + next).endsWith("one")) => (current + state.dropRight(2) + "1", "")
-      case ((current, state), next) if ((state + next).endsWith("two")) => (current + state.dropRight(2) + "2", "")
-      case ((current, state), next) if ((state + next).endsWith("three")) => (current + state.dropRight(4) + "3", "")
-      case ((current, state), next) if ((state + next).endsWith("four")) => (current + state.dropRight(3) + "4", "")
-      case ((current, state), next) if ((state + next).endsWith("five")) => (current + state.dropRight(3) + "5", "")
-      case ((current, state), next) if ((state + next).endsWith("six")) => (current + state.dropRight(2) + "6", "")
-      case ((current, state), next) if ((state + next).endsWith("seven")) => (current + state.dropRight(4) + "7", "")
-      case ((current, state), next) if ((state + next).endsWith("eight")) => (current + state.dropRight(4) + "8", "")
-      case ((current, state), next) if ((state + next).endsWith("nine")) => (current + state.dropRight(3) + "9", "")
-      case ((current, state), next) if (next.isDigit) => (current + state + next, "")
+    val (result, _) = line.foldLeft(("", "")) {
+      case ((current, state), next) if (next.isDigit) => (current + next, "")
+      case ((current, state), next) if ((state + next).endsWith("one")) => (current + "1", "")
+      case ((current, state), next) if ((state + next).endsWith("two")) => (current + "2", "")
+      case ((current, state), next) if ((state + next).endsWith("three")) => (current + "3", "")
+      case ((current, state), next) if ((state + next).endsWith("four")) => (current + "4", "")
+      case ((current, state), next) if ((state + next).endsWith("five")) => (current + "5", "")
+      case ((current, state), next) if ((state + next).endsWith("six")) => (current + "6", "")
+      case ((current, state), next) if ((state + next).endsWith("seven")) => (current + "7", "")
+      case ((current, state), next) if ((state + next).endsWith("eight")) => (current + "8", "")
+      case ((current, state), next) if ((state + next).endsWith("nine")) => (current + "9", "")
       case ((current, state), next) => (current, state + next)
     }
-    result._1 + result._2
+    result
 
 @main def main: Unit =
   val input = Source.fromFile("input/day1.txt").getLines().mkString("\n")
