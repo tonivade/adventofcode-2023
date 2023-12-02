a515f1b97ac6660d6094880140f39261e8ad7982
Antonio Muñoz <antoniogmc@gmail.com>
2023-12-02 15:32:59 +0100

    Day2 skeleton

---
+++ src/main/scala/Day2.scala
@@ -0,0 +1,13 @@
+package day2
+
+import scala.io.Source
+
+object Day2:
+  def part1(input: String): Int = ???
+  def part2(input: String): Int = ???
+
+@main def main: Unit =
+  val input = Source.fromFile("input/day2.txt").getLines().mkString("\n")
+  println(Day2.part1(input))
+  println(Day2.part2(input))
+
