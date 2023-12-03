e0c5aa20331511cedf854e110fc647fc06dc91a7
Antonio Muñoz <antoniogmc@gmail.com>
2023-12-03 09:59:55 +0100

    day 2 part 1

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -2,8 +2,43 @@ package day2
 
 import scala.io.Source
 
+enum Color {
+  case Red, Green, Blue
+}
+
+case class Subset(cubes: Map[Color, Int])
+case class Game(number: Int, subsets: List[Subset])
+
 object Day2:
-  def part1(input: String): Int = ???
+  def parse(line: String): Game =
+    val (head, tail) = line.split(":") match {
+      case Array(h, t) => (h, t)
+    }
+
+    val (_, number) = head.splitAt(4)
+
+    val cubes = tail.split(";").map(_.split(",").map(_.trim()).map(_.split(" ")).map{
+      case Array(h, "red") => Color.Red -> h.toInt
+      case Array(h, "blue") => Color.Blue -> h.toInt
+      case Array(h, "green") => Color.Green -> h.toInt
+    }).map((_.toMap)).map(Subset.apply).toList
+  
+    Game(number.trim().toInt, cubes)
+
+  def possible(red: Int, green: Int, blue: Int)(game: Game): Boolean =
+    game.subsets.filter(s => 
+      s.cubes.getOrElse(Color.Red, 0) > red ||
+      s.cubes.getOrElse(Color.Blue, 0) > blue ||
+      s.cubes.getOrElse(Color.Green, 0) > green
+      ).isEmpty
+
+  def part1(input: String): Int = 
+    input.split("\n")
+      .map(parse)
+      .filter(possible(12, 13, 14))
+      .map(_.number)
+      .sum
+
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day2Suite.scala
+++ src/test/scala/Day2Suite.scala
@@ -4,10 +4,15 @@ import Day2._
 
 class Day2Suite extends munit.FunSuite:
 
-  val input = """""".stripMargin
+  val input = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                |Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                |Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                |Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                |Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+                |""".stripMargin
 
   test("Day2 part1") {
-    assertEquals(part1(input), 1)
+    assertEquals(part1(input), 8)
   }
 
   test("Day2 part2") {
