313196b42cdb95e27ef3271e79d62794149dfa6e
Antonio Muñoz <antoniogmc@gmail.com>
2023-12-03 18:20:01 +0100

    Day3 part2

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -17,6 +17,8 @@ case class Position(x: Int, y: Int) {
 }
 
 case class PartNumber(pos: List[(Position, Char)]) {
+  def lastX: Int = pos.last._1.x
+
   def value: Int =
     pos.map(_._2).toList.mkString.toInt
 
@@ -30,29 +32,48 @@ case class PartNumber(pos: List[(Position, Char)]) {
 // https://adventofcode.com/2023/day/3
 object Day3:
 
-  def part1(input: String): Int = 
-    val numbers = input.split("\n")
+  def parsePartNumbers(input: String): List[PartNumber] = 
+    input.split("\n")
       .zipWithIndex.flatMap {
         case (line, y) => line.zipWithIndex.filter(_._1.isDigit).foldLeft(List.empty[PartNumber]) {
           case (Nil, (value, x)) => List(PartNumber(List((Position(x, y), value))))
-          case (head :: tail, (value, x)) if head.pos.last._1.x == x - 1 => head.add(Position(x, y), value) :: tail
-          case (head :: tail, (value, x)) if head.pos.last._1.x != x - 1 => PartNumber(List((Position(x, y), value))) :: head :: tail
+          case (head :: tail, (value, x)) if head.lastX == x - 1 => head.add(Position(x, y), value) :: tail
+          case (head :: tail, (value, x)) if head.lastX != x - 1 => PartNumber(List((Position(x, y), value))) :: head :: tail
           case (status, _) => status
         }
-      }
-
-    val symbols = input.split("\n")
+      }.toList
+  
+  def parseSymbols(input: String): List[Position] =
+    input.split("\n")
       .zipWithIndex.flatMap {
         case (line, y) => line.zipWithIndex.filterNot(_._1.isDigit).filterNot(_._1 == '.').map {
           case (_, x) => Position(x, y)
         }
       }.toList
+  
+  def parseGears(input: String): List[Position] =
+    input.split("\n")
+      .zipWithIndex.flatMap {
+        case (line, y) => line.zipWithIndex.filter(_._1 == '*').map {
+          case (_, x) => Position(x, y)
+        }
+      }.toList
 
+  def part1(input: String): Int = 
+    val numbers = parsePartNumbers(input)
+    val symbols = parseSymbols(input)
     numbers.filter(number => symbols.find(number.contains).isDefined)
       .map(_.value)
       .sum
 
-  def part2(input: String): Int = ???
+  def part2(input: String): Int = 
+    val numbers = parsePartNumbers(input)
+    val gears = parseGears(input)
+    gears
+      .map(symbol => numbers.filter(_.contains(symbol)).map(_.value))
+      .filter(_.size > 1)
+      .map(_.product)
+      .sum
 
 @main def main: Unit =
   val input = Source.fromFile("input/day3.txt").getLines().mkString("\n")
