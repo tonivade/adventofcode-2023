8e500e138b353679f073e14ca863c0a9db4220eb
Antonio Muñoz <antoniogmc@gmail.com>
2023-12-07 23:11:32 +0100

    Day5 part1

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -3,32 +3,41 @@ package day5
 import scala.io.Source
 
 case class Range(source: Long, destination: Long, size: Long):
-  def toMap: Map[Long, Long] =
-    (0.toLong until size).map(i => (i + source) -> (i + destination)).toMap
+  val endSource = source + size
+  def apply(i: Long): Option[Long] = 
+    if (i >= source && i < endSource) {
+      Some(destination + (i - source))
+    } else {
+      None
+    }
+
+case class Ranges(ranges: List[Range]):
+  def apply(i: Long): Long =
+    ranges.foldLeft(Option.empty[Long]) {
+      case (None, range) => range(i)
+      case (some, range) => some
+    }.getOrElse(i)
 
 // https://adventofcode.com/2023/day/5
 object Day5:
 
-  def parse(input: String): Map[Long, Long] = 
+  def parse(input: String): List[Range] = 
     input.split("\n")
       .drop(1)
       .map(_.split("\\s"))
       .map {
         case Array(a, b, c) => Range(b.toLong, a.toLong, c.toLong)
       }
-      .map(_.toMap)
-      .reduce(_ ++ _)
+      .toList
 
   def part1(input: String): Long = 
     val lines = input.split("\n\n")
 
     val (_, seeds) = lines(0).splitAt(lines(0).indexOf(":") + 1)
     val parsedSeeds = seeds.trim().split("\\s").map(_.toLong)
-    val maps = lines.drop(1).map(parse)
+    val ranges = lines.drop(1).map(parse).map(Ranges.apply)
 
-    println("parsed")
-
-    def get(i: Int)(j: Long) = maps(i).getOrElse(j, j)
+    def get(i: Int)(j: Long) = ranges(i)(j)
 
     val getAll = get(0) andThen 
       get(1) andThen 
