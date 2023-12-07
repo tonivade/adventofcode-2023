d19ad440c08a302b591061d581809b078dcf14df
Antonio Muñoz <antoniogmc@gmail.com>
2023-12-07 22:56:16 +0100

    day5 part1 brute force

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -2,9 +2,44 @@ package day5
 
 import scala.io.Source
 
+case class Range(source: Long, destination: Long, size: Long):
+  def toMap: Map[Long, Long] =
+    (0.toLong until size).map(i => (i + source) -> (i + destination)).toMap
+
 // https://adventofcode.com/2023/day/5
 object Day5:
-  def part1(input: String): Int = ???
+
+  def parse(input: String): Map[Long, Long] = 
+    input.split("\n")
+      .drop(1)
+      .map(_.split("\\s"))
+      .map {
+        case Array(a, b, c) => Range(b.toLong, a.toLong, c.toLong)
+      }
+      .map(_.toMap)
+      .reduce(_ ++ _)
+
+  def part1(input: String): Long = 
+    val lines = input.split("\n\n")
+
+    val (_, seeds) = lines(0).splitAt(lines(0).indexOf(":") + 1)
+    val parsedSeeds = seeds.trim().split("\\s").map(_.toLong)
+    val maps = lines.drop(1).map(parse)
+
+    println("parsed")
+
+    def get(i: Int)(j: Long) = maps(i).getOrElse(j, j)
+
+    val getAll = get(0) andThen 
+      get(1) andThen 
+      get(2) andThen 
+      get(3) andThen
+      get(4) andThen
+      get(5) andThen
+      get(6)
+
+    parsedSeeds.map(getAll).min
+
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day5Suite.scala
+++ src/test/scala/Day5Suite.scala
@@ -4,10 +4,42 @@ import Day5._
 
 class Day5Suite extends munit.FunSuite:
 
-  val input = """""".stripMargin
+  val input = """seeds: 79 14 55 13
+                |
+                |seed-to-soil map:
+                |50 98 2
+                |52 50 48
+                |
+                |soil-to-fertilizer map:
+                |0 15 37
+                |37 52 2
+                |39 0 15
+                |
+                |fertilizer-to-water map:
+                |49 53 8
+                |0 11 42
+                |42 0 7
+                |57 7 4
+                |
+                |water-to-light map:
+                |88 18 7
+                |18 25 70
+                |
+                |light-to-temperature map:
+                |45 77 23
+                |81 45 19
+                |68 64 13
+                |
+                |temperature-to-humidity map:
+                |0 69 1
+                |1 0 69
+                |
+                |humidity-to-location map:
+                |60 56 37
+                |56 93 4""".stripMargin
 
   test("Day5 part1") {
-    assertEquals(part1(input), 1)
+    assertEquals(part1(input), 35.toLong)
   }
 
   test("Day5 part2") {
