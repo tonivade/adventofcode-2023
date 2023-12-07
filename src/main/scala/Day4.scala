36a2f46407bf88da0e22d9eaba398ea1d17b06a7
Antonio Muñoz <antonio.munoz@clarity.ai>
2023-12-07 11:07:29 +0100

    Day4 part1

--- src/main/scala/Day4.scala
+++ src/main/scala/Day4.scala
@@ -2,9 +2,33 @@ package day4
 
 import scala.io.Source
 
+case class Card(number: Int, winningNumbers: Set[Int], numbers: Set[Int]):
+  def points: Int = numbers.intersect(winningNumbers).size match {
+    case i if i == 0 => 0
+    case i => Math.pow(2, i - 1).toInt
+  }
+
 // https://adventofcode.com/2023/day/4
 object Day4:
-  def part1(input: String): Int = ???
+  val regex = "\\b(\\d+)\\b".r
+
+  def parse(line: String): Card =
+    val (card, (winningNumbers, numbers)) = line.splitAt(line.indexOf(':')) match {
+      case (left, right) => (left.filter(_.isDigit).toInt, right.splitAt(right.indexOf('|')))
+    }
+
+    Card(card,
+      regex.findAllIn(winningNumbers)
+        .map(_.toInt).toSet,
+      regex.findAllIn(numbers)
+        .map(_.toInt).toSet)
+
+  def part1(input: String): Int =
+    input.split("\n")
+      .map(parse)
+      .map(_.points)
+      .sum
+
   def part2(input: String): Int = ???
 
 @main def main: Unit =
