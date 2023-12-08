eb2847d3e6fc8b8ef8cd0671ab8551f5ccd8cbee
Antonio Muñoz <antoniogmc@gmail.com>
2023-12-09 00:11:22 +0100

    Day6 skeleton

---
+++ src/main/scala/Day6.scala
@@ -0,0 +1,14 @@
+package day6
+
+import scala.io.Source
+
+// https://adventofcode.com/2023/day/6
+object Day6:
+  def part1(input: String): Int = ???
+  def part2(input: String): Int = ???
+
+@main def main: Unit =
+  val input = Source.fromFile("input/day6.txt").getLines().mkString("\n")
+  println(Day6.part1(input))
+  println(Day6.part2(input))
+
---
+++ src/test/scala/Day6Suite.scala
@@ -0,0 +1,16 @@
+package day6
+
+import Day6._
+
+class Day6Suite extends munit.FunSuite:
+
+  val input = """""".stripMargin
+
+  test("Day6 part1") {
+    assertEquals(part1(input), 1)
+  }
+
+  test("Day6 part2") {
+    assertEquals(part2(input), 1)
+  }
+
