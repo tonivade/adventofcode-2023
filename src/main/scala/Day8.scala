449cbedf5775bf732afe0175662e19fd3c6b66fc
Antonio Muñoz <antoniogmc@gmail.com>
2023-12-09 18:11:29 +0100

    Day8 part1 WIP

--- src/main/scala/Day8.scala
+++ src/main/scala/Day8.scala
@@ -1,10 +1,39 @@
 package day8
 
 import scala.io.Source
+import scala.annotation.tailrec
+
+enum Step:
+  case Left, Right
+
+case class Node(label: String, left: String, right: String)
 
 // https://adventofcode.com/2023/day/8
 object Day8:
-  def part1(input: String): Int = ???
+  val regex = "([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)".r
+
+  @tailrec
+  def walk(map: Map[String, Node], steps: List[Step], current: String, count: Int = 0): (String, Int) =
+    steps match {
+      case Step.Left :: tail => walk(map, tail, map(current).left, count + 1)
+      case Step.Right :: tail => walk(map, tail, map(current).right, count + 1)
+      case Nil => (current, count)
+    }
+
+  def part1(input: String): Int = 
+    val lines = input.split("\n")
+    val steps = lines(0).map {
+      case 'L' => Step.Left
+      case 'R' => Step.Right
+    }.toList
+    val map = lines.drop(2).map {
+      case regex(label, left, right) => label -> Node(label, left, right)
+    }.toMap
+
+    val result = walk(map, steps, "AAA")
+    println(result)
+
+    ???
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day8Suite.scala
+++ src/test/scala/Day8Suite.scala
@@ -4,7 +4,15 @@ import Day8._
 
 class Day8Suite extends munit.FunSuite:
 
-  val input = """""".stripMargin
+  val input = """RL
+                |
+                |AAA = (BBB, CCC)
+                |BBB = (DDD, EEE)
+                |CCC = (ZZZ, GGG)
+                |DDD = (DDD, DDD)
+                |EEE = (EEE, EEE)
+                |GGG = (GGG, GGG)
+                |ZZZ = (ZZZ, ZZZ)""".stripMargin
 
   test("Day8 part1") {
     assertEquals(part1(input), 1)
