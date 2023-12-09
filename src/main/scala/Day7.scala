7675bd04a412ce97f0166cfd8311a70ba9dd5b8c
Antonio Muñoz <antoniogmc@gmail.com>
2023-12-09 17:09:57 +0100

    Day7 part1

--- src/main/scala/Day7.scala
+++ src/main/scala/Day7.scala
@@ -2,9 +2,84 @@ package day7
 
 import scala.io.Source
 
+enum Card:
+  case Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two
+
+enum HandType:
+  case FiveOfKind, FourOfKind, FullHouse, ThreeOfKind, TwoPair, OnePair, HighCard
+
+case class Hand(c1: Card, c2: Card, c3: Card, c4: Card, c5: Card):
+  def toList: List[Card] = List(c1, c2, c3, c4, c5)
+  def handType: HandType = toList.groupBy(identity).mapValues(_.size).values.toList.sorted.reverse match {
+      case List(5) => HandType.FiveOfKind
+      case List(4, 1) => HandType.FourOfKind
+      case List(3, 2) => HandType.FullHouse
+      case List(3, 1, 1) => HandType.ThreeOfKind
+      case List(2, 2, 1) => HandType.TwoPair
+      case List(2, 1, 1, 1) => HandType.OnePair
+      case _ => HandType.HighCard
+    }
+
+implicit object HandOrdering extends Ordering[Hand]:
+  def compare(a: Hand, b: Hand): Int = 
+    var x = a.handType.ordinal compareTo b.handType.ordinal
+    if (x == 0)
+      x = a.c1.ordinal compareTo b.c1.ordinal
+      if (x == 0)
+        x = a.c2.ordinal compareTo b.c2.ordinal
+        if (x == 0)
+          x = a.c3.ordinal compareTo b.c3.ordinal
+          if (x == 0)
+            x = a.c4.ordinal compareTo b.c4.ordinal
+            if (x == 0)
+              a.c4.ordinal compareTo b.c4.ordinal
+            else
+              x
+          else
+            x
+        else
+          x
+      else
+        x
+    else
+      x
+
 // https://adventofcode.com/2023/day/7
 object Day7:
-  def part1(input: String): Int = ???
+  import Card._
+
+  def toCard(card: Char): Card =
+    card match {
+      case 'A' => Ace
+      case 'K' => King
+      case 'Q' => Queen
+      case 'J' => Jack
+      case 'T' => Ten
+      case '9' => Nine
+      case '8' => Eight
+      case '7' => Seven
+      case '6' => Six
+      case '5' => Five
+      case '4' => Four
+      case '3' => Three
+      case '2' => Two
+    }
+
+  def parse(input: String): Hand = 
+    input.map(toCard) match {
+      case Seq(c1, c2, c3, c4, c5) => Hand(c1, c2, c3, c4, c5)
+    }
+    
+  def part1(input: String): Int = 
+    input.split("\n")
+      .map(line => (parse(line.take(5)), line.drop(6).toInt))
+      .sorted
+      .reverse
+      .zipWithIndex
+      .map {
+        case ((hand, bid), i) => (i + 1) * bid
+      }.sum
+
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day7Suite.scala
+++ src/test/scala/Day7Suite.scala
@@ -4,10 +4,14 @@ import Day7._
 
 class Day7Suite extends munit.FunSuite:
 
-  val input = """""".stripMargin
+  val input = """32T3K 765
+                |T55J5 684
+                |KK677 28
+                |KTJJT 220
+                |QQQJA 483""".stripMargin
 
   test("Day7 part1") {
-    assertEquals(part1(input), 1)
+    assertEquals(part1(input), 6440)
   }
 
   test("Day7 part2") {
