04deb222e009eb61441535493ea48a27a40f3a8e
Antonio Muñoz <antoniogmc@gmail.com>
2023-12-09 15:57:35 +0100

    Day1 part2

--- src/main/scala/Day6.scala
+++ src/main/scala/Day6.scala
@@ -2,9 +2,29 @@ package day6
 
 import scala.io.Source
 
+case class Race(time: Int, distance: Int):
+  def waysToWin: Seq[Int] = all.filter(_._2 > distance).map(_._1)
+  def all: Seq[(Int, Int)] =
+    for {
+      i <- 0 to time
+    } yield (i, calculate(i))
+  def calculate(hold: Int): Int = (time - hold) * hold
+
 // https://adventofcode.com/2023/day/6
 object Day6:
-  def part1(input: String): Int = ???
+  val regex = "\\b(\\d+)\\b".r
+
+  def parse(line: String): List[Int] = regex.findAllIn(line).map(_.toInt).toList
+
+  def part1(input: String): Int = 
+    val (times, distances) = input.split("\n") match {
+      case Array(a, b) => (parse(a), parse(b))
+    }
+
+    val races = (0 until times.size).map(i => Race(times(i), distances(i)))
+
+    races.map(_.waysToWin.size).product
+
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day6Suite.scala
+++ src/test/scala/Day6Suite.scala
@@ -4,10 +4,11 @@ import Day6._
 
 class Day6Suite extends munit.FunSuite:
 
-  val input = """""".stripMargin
+  val input = """Time:      7  15   30
+                |Distance:  9  40  200""".stripMargin
 
   test("Day6 part1") {
-    assertEquals(part1(input), 1)
+    assertEquals(part1(input), 288)
   }
 
   test("Day6 part2") {
