a6b0bee9c5a1ab625664069742f5cedd36c0c707
Antonio Muñoz <antoniogmc@gmail.com>
2023-12-10 21:43:00 +0100

    Day10 skeleton

---
+++ src/main/scala/Day10.scala
@@ -0,0 +1,14 @@
+package day10
+
+import scala.io.Source
+
+// https://adventofcode.com/2023/day/10
+object Day10:
+  def part1(input: String): Int = ???
+  def part2(input: String): Int = ???
+
+@main def main: Unit =
+  val input = Source.fromFile("input/day10.txt").getLines().mkString("\n")
+  println(Day10.part1(input))
+  println(Day10.part2(input))
+
---
+++ src/test/scala/Day10Suite.scala
@@ -0,0 +1,16 @@
+package day10
+
+import Day10._
+
+class Day10Suite extends munit.FunSuite:
+
+  val input = """""".stripMargin
+
+  test("Day10 part1") {
+    assertEquals(part1(input), 1)
+  }
+
+  test("Day10 part2") {
+    assertEquals(part2(input), 1)
+  }
+
