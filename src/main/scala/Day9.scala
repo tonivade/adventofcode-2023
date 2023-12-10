86fa1dd7d9125941484fe180fe50b557f102baf5
Antonio Muñoz <antoniogmc@gmail.com>
2023-12-10 21:35:34 +0100

    Day9 part1

--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -1,10 +1,36 @@
 package day9
 
 import scala.io.Source
+import scala.annotation.tailrec
 
 // https://adventofcode.com/2023/day/9
 object Day9:
-  def part1(input: String): Int = ???
+  val regex = "-?\\d+".r
+
+  @tailrec
+  def reduce(input: List[Int], state: List[List[Int]] = List.empty): List[List[Int]] =
+    val next = input.sliding(2).map {
+      case List(a, b) => b - a
+    }.toList
+    if (next.forall(_ == 0))
+      next :: state
+    else 
+      reduce(next, next :: state)
+
+  @tailrec
+  def infer(input: List[List[Int]], next: Int = 0): Int =
+    input match {
+      case head :: tail => infer(tail, head.last + next)
+      case Nil => next
+    }
+
+  def part1(input: String): Int =
+    input.split("\n")
+      .map(line => regex.findAllIn(line).map(_.toInt).toList)
+      .map(l => reduce(l) :+ l)
+      .map(l => infer(l))
+      .sum
+
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day9Suite.scala
+++ src/test/scala/Day9Suite.scala
@@ -4,10 +4,12 @@ import Day9._
 
 class Day9Suite extends munit.FunSuite:
 
-  val input = """""".stripMargin
+  val input = """0 3 6 9 12 15
+                |1 3 6 10 15 21
+                |10 13 16 21 30 45""".stripMargin
 
   test("Day9 part1") {
-    assertEquals(part1(input), 1)
+    assertEquals(part1(input), 114)
   }
 
   test("Day9 part2") {
