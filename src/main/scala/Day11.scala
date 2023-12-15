53d217ff630560b84847d75f7bf80209c33795c4
Antonio Muñoz <antoniogmc@gmail.com>
2023-12-15 18:26:58 +0100

    Day11 skeleton

---
+++ src/main/scala/Day11.scala
@@ -0,0 +1,14 @@
+package day11
+
+import scala.io.Source
+
+// https://adventofcode.com/2023/day/11
+object Day11:
+  def part1(input: String): Int = ???
+  def part2(input: String): Int = ???
+
+@main def main: Unit =
+  val input = Source.fromFile("input/day11.txt").getLines().mkString("\n")
+  println(Day11.part1(input))
+  println(Day11.part2(input))
+
---
+++ src/test/scala/Day11Suite.scala
@@ -0,0 +1,16 @@
+package day11
+
+import Day11._
+
+class Day11Suite extends munit.FunSuite:
+
+  val input = """""".stripMargin
+
+  test("Day11 part1") {
+    assertEquals(part1(input), 1)
+  }
+
+  test("Day11 part2") {
+    assertEquals(part2(input), 1)
+  }
+
